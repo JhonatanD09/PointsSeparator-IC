9a6a7e56e7a62c0ff9bc1fb61416bf1f1fa44edc
JhonatanD09 <joranada@gmail.com>
2021-08-26 12:02:24 -0500

    v2.0

--- src/models/PointSeparator.java
+++ src/models/PointSeparator.java
@@ -1,5 +1,11 @@
 package models;
 
+/**
+ * Clase encargada del funcionamiento de la red neuronal
+ * para aprender a diferenciar los conjuntos de puntos
+ * @author jhona
+ *
+ */
 public class PointSeparator {
 
 		public static double APRENDIZAJE = 0.2;
@@ -9,6 +15,10 @@ public class PointSeparator {
 			      								  {2,0,0} ,{3,0,0} };
 		public double pesoN1X1, pesoN1X2, pesoN2X1, pesoN2X2, baiasN1, baiasN2;
 
+		/**
+		 * Constructor de la clase
+		 * aqui se inicializan los valores de los pesos para empezar a iterarlos
+		 */
 		public PointSeparator() {
 			pesoN1X1 = 0;
 			pesoN1X2 = 0;
@@ -18,6 +28,11 @@ public class PointSeparator {
 			baiasN2 = 0.05;
 		}
 
+		/**
+		 * Funcion de activacion hardlimit
+		 * @param value valor a evaluar con hardlimit
+		 * @return 0 o 1 segun corresponda
+		 */
 		private int hardLimit(double value) {
 			if (value >= 0) {
 				return 1;
@@ -26,6 +41,12 @@ public class PointSeparator {
 			}
 		}
 
+		/**
+		 * Metodo encargado de realizar el aprendizje de la 
+		 * neurona
+		 * llama a la funcion evaluar pesos
+		 * 
+		 */
 		public void aprendizaje() {
 			System.out.println("Iniciando Aprendizaje....");
 			for (int i = 0; i < 10; i++) {
@@ -35,6 +56,9 @@ public class PointSeparator {
 			imprimirSalida();
 		}
 
+		/**
+		 * Formatea los datos que le llegan a la neurona de salida y los muestra por consola
+		 */
 		private void imprimirSalida() {
 			for (int i = 0; i < dataSetNeurona1.length; i++) {
 				int hardLimitN1 = hardLimit(dataSetNeurona1[i][0] * pesoN1X1 + dataSetNeurona1[i][1] * pesoN1X2 - baiasN1);
@@ -44,13 +68,29 @@ public class PointSeparator {
 				}
 		}
 
+		/**
+		 * se encarga de evaluar las entradas de la neurona de salida
+		 * para dar el valor esperado de ellas
+		 * los parametros son las entradas de la neurona
+		 * @param x1
+		 * @param x2
+		 * @return el valor esperado con dichas entradas
+		 */
 		private String resultado(int x1, int x2) {
 			return x1==1&&x2==0||x1==0&&x2==1?"A":"B";
 		}
 		
+		/**
+		 * Se encarga de evaluar el los data set de cada neurona, con ello
+		 * evalua cuando hay un error y re calcula los valores de los pesos y el baias dependiendo
+		 * del factor de aprendizaje
+		 * Conecta con la neurona de salida mediante el metodo imprimirSalida
+		 * @param matriz data set de la neurona 1
+		 * @param matriz2  data set de la neurona 2
+		 */
 		private void evaluarPesos(int[][] matriz, int[][] matriz2) {
 			
-//			imprimirSalida();
+			imprimirSalida();
 			System.out.println("-------------");
 			for (int i = 0; i < matriz.length; i++) {
 				
@@ -79,10 +119,13 @@ public class PointSeparator {
 				pesoN2X2 = pesoN2X2 + delta4;
 				baiasN2 = baiasN2 - (APRENDIZAJE * error2);
 //				
-//				System.out.println("----------------------------------");
+				System.out.println("----------------------------------");
 			}
 		}
 
+		/**
+		 * Metodo encargado de mostrar por consola los valores de las ecuaciones de las neuronas
+		 */
 		public void mostrarEcuaciones() {
 			System.out.println("Ecuacion de la neurona 1: " + pesoN1X1 + " * X1 + " + pesoN1X2 + " * X2 - " + baiasN1);
 			System.out.println("Ecuacion de la neurona 2: " + pesoN2X1 + " * X1 + " + pesoN2X2 + " * X2 - " + baiasN2);
